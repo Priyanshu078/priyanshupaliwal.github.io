2a5838bee215bc738a537b1d7ff47064d36bc270
Priyanshu078 <priyanshupaliwal078@gmail.com>
2022-03-13 14:56:08 +0530

    added resume to the website

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
-"assets/NOTICES": "2ceedb153a9ddf191c47e7cb8b46fda6",
+"assets/NOTICES": "36090f27aea0bec01bde94790bac8997",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "7efa5340123de28cf6d2025892130ee7",
-"/": "7efa5340123de28cf6d2025892130ee7",
-"main.dart.js": "d4da9e55136e1fc39a87e72313a748f8",
+"index.html": "1fe8d74d2ce3538de9f40c4f68e5e21e",
+"/": "1fe8d74d2ce3538de9f40c4f68e5e21e",
+"main.dart.js": "f08e9abf20f3ab3dd0b31fa62561a438",
 "manifest.json": "dd0b809d1b564db27abde691f132b229",
 "version.json": "5ba19ed63682788b8ff8f8af2052800e"
 };
